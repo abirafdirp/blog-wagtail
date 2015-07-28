9cbe152e2e709d1bb25e2b72c45792acafedd474
abirafdirp <abirafdiraditya@gmail.com>
2015-07-28 19:42:42 +0700

    m

--- home/models.py
+++ home/models.py
@@ -6,6 +6,7 @@ from wagtail.wagtailcore.models import Page
 from wagtail.wagtailcore.fields import RichTextField
 from wagtail.wagtailadmin.edit_handlers import FieldPanel
 
+
 class HomePage(Page):
     overview = RichTextField(blank=True)
 
