2ab51d958941ef4f81c741c3c1cbd264ea0a8232
abirafdi raditya putra <abirafdirp@yahoo.com>
2015-08-01 01:21:12 +0700

    -index is now angular. -post design is final, working to convert to angular

--- blog-wagtail/static/js/controllers.js
+++ blog-wagtail/static/js/controllers.js
@@ -2,14 +2,14 @@ var blogControllers = angular.module('blogControllers', []);
 
 blogControllers.controller('blogIndexCtrl', ['$scope', 'BlogIndex',
   function($scope, BlogIndex) {
-    $scope.imageID = 0;
-    $scope.blogposts = BlogIndex.posts.query();
-    $scope.setImageID = function(ID) {
-      $scope.imageID = ID;
-      console.log($scope.imageID);
+    $scope.images = BlogIndex.images.query();
+    $scope.posts = BlogIndex.posts.query();
+    $scope.getImage = function(id) {
+      for(var i = 0; i < $scope.images.images.length; i++){
+        if($scope.images.images[i].id === id){
+          return $scope.images.images[i].file;
+        }
+      }
     }
-    $scope.$watch('imageID', function() {
-      $scope.image = BlogIndex.image.query({id: $scope.imageID})
-    });
   }
 ]);
--- blog-wagtail/static/js/services.js
+++ blog-wagtail/static/js/services.js
@@ -6,7 +6,7 @@ blogServices.factory('BlogIndex', ['$resource',
       posts : $resource('/api/v1/pages/?type=blog.BlogPostPage&fields=title,date,main_image', {}, {
         query: {method: 'GET', isArray: false}
       }),
-      image : $resource('/api/v1/images/:id/?fields=file', {id:'@id'}, {
+      images : $resource('/api/v1/images/?fields=file&tags=index', {}, {
         query: {method: 'GET', isArray: false}
       })
     }
