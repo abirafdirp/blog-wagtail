bfb06881e9a4a2d34dc42d8a121f1665409a839d
abirafdirp <abirafdiraditya@gmail.com>
2015-08-16 16:05:57 +0700

    force https django-sslify

--- config/settings/common.py
+++ config/settings/common.py
@@ -77,6 +77,7 @@ INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + WAGTAIL + LOCAL_APPS
 # ------------------------------------------------------------------------------
 MIDDLEWARE_CLASSES = (
     # Make sure djangosecure.middleware.SecurityMiddleware is listed first
+    'sslify.middleware.SSLifyMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- config/settings/local.py
+++ config/settings/local.py
@@ -62,3 +62,4 @@ TEST_RUNNER = 'django.test.runner.DiscoverRunner'
 # Your local stuff: Below this line define 3rd party library settings
 
 COMPRESS_ENABLED = False
+SSLIFY_DISABLE = True
